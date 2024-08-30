f5a1557fc78fd2e62d8f259cb2cc181ed758a935
thinkie72 <tyler.hinkie@menloschool.org>
2024-08-29 22:48:46 -0700

    Finished all of Riddler and commented all parts.

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,10 +1,11 @@
 Name: YOUR NAME HERE
 
-| Date   |   Time    |                                                        Update |
-|:-------|:---------:|--------------------------------------------------------------:|
-| Aug 27 | 8-8:30pm  | I began coding the first section to reflect the Caeser shift. |
-| Aug 27 | 8:30-9pm  |                                     Finished Riddler 1 and 2. |
-| Aug 29 | 9:30-10pm |                    Finished Riddler 3 and began commenting 1. |
+| Date   |    Time    |                                                        Update |
+|:-------|:----------:|--------------------------------------------------------------:|
+| Aug 27 |  8-8:30pm  | I began coding the first section to reflect the Caeser shift. |
+| Aug 27 |  8:30-9pm  |                                     Finished Riddler 1 and 2. |
+| Aug 29 | 9:30-10pm  |                    Finished Riddler 3 and began commenting 1. |
+| Aug    | 10-10:45pm |              Finished all of Riddler and commented all parts. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Riddler.java
+++ src/Riddler.java
@@ -32,8 +32,10 @@ public class Riddler {
 
     public String decryptTwo(String encrypted) {
         StringBuilder decrypted = new StringBuilder();
+        // Splits the list of numbers into an array by space separation
         String[] numbers = encrypted.split(" ");
         for (int i = 0; i < numbers.length; i++) {
+            // Cast ints to chars to add to decrypted to complete the message
             decrypted.append(((char) Integer.parseInt(numbers[i])));
         }
         System.out.println(decrypted);
@@ -42,8 +44,11 @@ public class Riddler {
 
     public String decryptThree(String encrypted) {
         StringBuilder decrypted = new StringBuilder();
+        // Number of binary bits equivalent to the proper message
+        // This was also important for easy transfer during testing
         int numBits = 8;
         for (int i = 0; i < encrypted.length() - numBits; i += numBits) {
+            // Use parseInt() to find the base 10 equivalent to the binary string and then cast that to a char
             decrypted.append((char) (Integer.parseInt(encrypted.substring(i, i + numBits), 2)));
         }
         System.out.println(decrypted);
@@ -51,10 +56,15 @@ public class Riddler {
     }
 
     public String decryptFour(String encrypted) {
-        String decrypted = "";
-
-        // TODO: Complete the decryptFour() function.
-
-        return decrypted;
+        StringBuilder decrypted = new StringBuilder();
+        // A char[] to prevent calling toCharArray() repeatedly
+        char[] dingbats = encrypted.toCharArray();
+        for (int i = 0; i < dingbats.length; i++) {
+            // Takes int equivalent to dingbat hex number and subtracts the difference of the starting dingbat unicode
+            // number and the start of the alphabet at 65
+            decrypted.append((char) ((int) dingbats[i] - 9919));
+        }
+        System.out.println(decrypted);
+        return decrypted.toString();
     }
 }
